616345374ebea322821b63735b2d0c4a975e2e93
alebs10 <alexandre2002.ab@gmail.com>
2022-05-24 22:36:12 -0300

    Criando componentes com React.

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://criticalhits.com.br/wp-content/uploads/2022/03/Shenlong-Dragon-Ball-capa-1-768x401.jpg" className='img' alt="sheng" />
+        </>
+    );
+}
+
+export default Home;
